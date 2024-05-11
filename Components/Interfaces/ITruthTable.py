2db248d47a1be1e4b07ac1e77ed34ef439427561
jka44566 <ben@fern.com.au>
2024-05-11 21:47:53 +1000

    truth table

--- Components/Interfaces/ITruthTable.py
+++ Components/Interfaces/ITruthTable.py
@@ -1,15 +1,22 @@
+from Components.Interfaces.ILogicalSentence import ILogicalSentence
 from Components.Interfaces.IPropositionalLogic import IPropositionalLogic
 
 
 class ITruthTable(IPropositionalLogic):
-    def __init__(self, knowledgeBase):
+    def __init__(self, knowledgeBase, ):
         self.knowledgeBase = knowledgeBase
 
     def _entails(self, alphabetic):
-        pass
+        symbols = self.knowledgeBase.symbols
+        for symbol in alphabetic.symbols:
+            if symbol not in symbols:
+                symbols.append(symbol)
+        return self._entails(alphabetic, symbols, {})
 
     def _inspect_entail(self, alphabetic, symbols, model):
         pass
 
     def evaluate(self, ask):
         pass
+
+    
