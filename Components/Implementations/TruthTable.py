3f23b83ef22a50668dea60d099c03e6b9e3f4cad
cobeo2004 <minh030963@stu.vinschool.edu.vn>
2024-05-13 15:46:11 +1000

    Fix Bug

--- Components/Implementations/TruthTable.py
+++ Components/Implementations/TruthTable.py
@@ -7,15 +7,12 @@ class TruthTable(ITruthTable):
     def __init__(self, knowledgeBase):
         super().__init__(knowledgeBase)
 
-    def _entails(self, alphabetic):
+    def _inspect_entail(self, alphabetic, symbols, model):
+        return super()._inspect_entail(alphabetic, symbols, model)
+
+    def entails(self, alphabetic):
         symbols = self.knowledgeBase.symbols
         for symbol in alphabetic.symbols:
             if symbol not in symbols:
                 symbols.append(symbol)
         return self._entails(alphabetic, symbols, {})
-
-    def _inspect_entail(self, alphabetic, symbols, model):
-        return super()._inspect_entail(alphabetic, symbols, model)
-
-    def evaluate(self, query):
-        return super().evaluate(query)
--- Components/Interfaces/ITruthTable.py
+++ Components/Interfaces/ITruthTable.py
@@ -1,16 +1,13 @@
 from Components.Interfaces.ILogicalSentence import ILogicalSentence
-from Components.Interfaces.IPropositionalLogic import IPropositionalLogic
+from Components.Interfaces.IPropositionalLogic import IPropositionalMethod
 
 
-class ITruthTable(IPropositionalLogic):
+class ITruthTable(IPropositionalMethod):
     def __init__(self, knowledgeBase):
         self.knowledgeBase = knowledgeBase
-
-    def _entails(self, alphabetic):
-        pass
 
     def _inspect_entail(self, alphabetic, symbols, model):
         pass
 
-    def evaluate(self, query):
+    def entails(self, query):
         pass
